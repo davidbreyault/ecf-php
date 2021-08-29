6be8f23bffc77f9ba66e9293c4075871b234db25
David Breyault <davidbreyault@gmail.com>
2021-08-29 19:12:06 +0200

    Mise à jour de la fonctionnalité d'upload de fichiers

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UploadController extends AbstractController
 {
@@ -34,12 +36,28 @@ class UploadController extends AbstractController
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
-                $file = $upload->getName();
-                $fileName = 'cv-'.strtolower($user->getFirstname()).'-'.strtolower($user->getLastname()).'-'.md5(uniqid()).'.'.$file->guessExtension();
-                $file->move($this->getParameter('upload_directory', $fileName));
-                $upload->setName($fileName);
-                $upload->setUser($user);
-    
+                $file = $form->get('name')->getData();
+                // this condition is needed because the 'brochure' field is not required
+                // so the PDF file must be processed only when a file is uploaded
+                if ($file) {
+                    $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                    $newFilename = 'cv-'.strtolower($user->getFirstname()).'-'.strtolower($user->getLastname()).'-'.uniqid().'.'.$file->guessExtension();
+
+                    // Move the file to the directory where brochures are stored
+                    try {
+                        $file->move(
+                            $this->getParameter('upload_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    // updates the 'brochureFilename' property to store the PDF file name, instead of its contents
+                    $upload->setName($newFilename);
+                    $upload->setUser($user);
+                }
+                
+                // ... persist the $upload variable or any other work
                 $this->entityManager->persist($upload);
                 $this->entityManager->flush();
                 $this->addFlash('success', 'Merci ! Votre fichier a bien été pris en compte !');
